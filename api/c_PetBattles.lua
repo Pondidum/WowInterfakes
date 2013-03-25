a12affa03e1f69e3cb9c04fd64fdb2dde09aa0d3
andy <andy@stormbase.net>
2013-03-25 21:15:52 +0000

    api: added pet battle namespace

---
+++ api/c_PetBattles.lua
@@ -0,0 +1,10 @@
+local petBattles = {
+	GetAllEffectNames = function()
+		return "", "", ""
+	end,	
+	GetAllStates = function()
+		return "", "", ""
+	end,
+}
+
+Api.export("C_PetBattles", petBattles)
