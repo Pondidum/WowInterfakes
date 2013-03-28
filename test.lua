652fe10c553e286e079657f3e79031989b96f7a4
andy <andy@stormbase.net>
2013-03-28 12:26:28 +0000

    test: updated test harness

--- test.lua
+++ test.lua
@@ -4,14 +4,9 @@ Api.debug.enabled = true
 
 local loader = Api.AddonLoader.new()
 
---loader.addonBase = "D:\\dev\\tools\\lua\\5.1\\lua\\wowinterfakes\\wow-ui-source"
---loader.addons = {"FrameXML"}
 loader.loadBlizzard()
+
+--loader.addonBase = "D:\\dev\\tools\\lua\\5.1\\lua\\wowinterfakes\\wow-ui-source"
+--loader.addons = {"Dark.Core", "Dark.Nameplates", "Dark.Unitframes", "Dark.Raids", "Dark.Warface"}
 --loader.load()
 
-print("Done.")
--- local loader = Api.AddonLoader.new("C:\\Program Files (x86)\\World of Warcraft\\Interface\\AddOns")
--- loader.addons = {"Dark.Core", "Dark.Nameplates", "Dark.Unitframes", "Dark.Raids", "Dark.Warface"}
--- loader.load()
-
---print("Dark:", Dark)
