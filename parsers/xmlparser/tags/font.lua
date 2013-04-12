02ed9b19341a3f75390d05750b835dfea527f73e
andy <andy@stormbase.net>
2013-04-13 00:15:32 +0100

    tags: added highlightFont

--- parsers/xmlparser/tags/font.lua
+++ parsers/xmlparser/tags/font.lua
@@ -5,4 +5,5 @@ local customise = function(this)
 end
 
 Api.parsers.xml.addTag("Font", customise)
+Api.parsers.xml.addTag("HighlightFont", customise)
 Api.parsers.xml.addTag("NormalFont", customise)
