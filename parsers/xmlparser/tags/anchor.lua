65038a58c8bcaa4c752892f9ba568ac4e9b4c14d
andy <andy@stormbase.net>
2013-04-17 13:07:54 +0100

    anchor: fix attribute values due to blizzard bug

--- parsers/xmlparser/tags/anchor.lua
+++ parsers/xmlparser/tags/anchor.lua
@@ -1,11 +1,20 @@
+local fixValue = function(value)
+	
+	if value == nil then
+		return nil
+	end
+
+	return value:gsub("%a", "")
+end
+
 Api.parsers.xml.addTag("Anchor", function(this)
 
 	this.processChildren = false
 	this.run = function(parent, element)
 
 		local point = element.point or ""
-		local x = element.x or 0
-		local y = element.y or 0
+		local x = fixValue(element.x) or 0
+		local y = fixValue(element.y) or 0
 
 		this.builder.append('%s:SetPoint("%s", %s, %s)', parent.variable, point, x, y)
 
