4245b1b469114d93cacc822da240b5422747ad25
andy <andy@stormbase.net>
2013-04-18 13:11:03 +0100

    fontString: added implementation

--- parsers/xmlparser/tags/fontString.lua
+++ parsers/xmlparser/tags/fontString.lua
@@ -3,7 +3,26 @@ local customise = function(this)
 	this.run = function(parent, element)
 	
 		this.buildVariable(element)
-		
+	
+		local parentVariable = "UIParent"		
+		local layer = 'nil'
+		local template = 'nil'
+
+		if element.layer then
+			layer = '"' .. element.layer .. '"'
+		end
+
+		if element.inherits then
+			template = '"' .. element.inherits .. '"'
+		end
+
+
+		if parent.variable then
+			parentVariable = parent.variable
+		end
+
+		this.builder.append('local %s = %s:CreateFontString(%s, %s, %s)', element.variable, parentVariable, this.buildName(element), layer, template)
+
 	end
 
 end
