2ff66253a8b740cfce639ce961c6a52e6e05c9f9
andy <andy@stormbase.net>
2013-06-06 13:20:49 +0100

    api: added getMovieDownloadProgress

---
+++ api/getMovieDownloadProgress.lua
@@ -0,0 +1,8 @@
+local getMovieDownloadProgress = function(movieId)
+	
+	--local inProgress, downloaded, total
+	return nil, 10, 0	
+end
+
+Api.export("GetMovieDownloadProgress", getMovieDownloadProgress)
+
