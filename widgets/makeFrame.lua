95839edaf86472f45da4ecd69c14627520911cbb
andy <andy@andydote.co.uk>
2013-11-09 20:17:02 +0000

    widgets.makeFrame: add RegisterEvent

--- widgets/makeFrame.lua
+++ widgets/makeFrame.lua
@@ -1,10 +1,15 @@
 local ns = ...
 local builder = ns.builder
+local eventRegistry = ns.eventRegistry
 
 builder.applyFrame = function(region) 
 
 	region.SetBackdrop = function(self, backdrop)
 		self.__storage.backdrop = backdrop
+	end
+
+	region.RegisterEvent = function(self, event)
+		eventRegistry.register(self, event)
 	end
 
 end
