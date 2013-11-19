3a9f60fbeb25625b914fac32c7eb3970122b6ff8
andy <andy@andydote.co.uk>
2013-11-19 00:15:42 +0000

    templateManager: log template registering, warn about non-existing templates

--- core/templateManager.lua
+++ core/templateManager.lua
@@ -1,10 +1,13 @@
 local ns = ...
+local log = ns.log:new("templateManager")
 
 local templates = {}
 
 local templateManager = {
 
 	addTemplate = function(name, decoratorChain)
+
+		log.debug("registering template", name)
 		templates[name] = decoratorChain
 	end,
 
@@ -17,6 +20,10 @@ local templateManager = {
 			for i, decorator in ipairs(decoratorChain) do
 				decorator(target)
 			end
+
+		else
+
+			log.warn("unable to find template", name)
 		end
 
 	end,
