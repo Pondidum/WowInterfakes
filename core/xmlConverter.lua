df8d9261ee5a0cd955dcc0624a1b37b10e4bb42e
andy <andy@andydote.co.uk>
2013-11-23 22:55:34 +0000

    xmlConverter: make a more sensible xml element

---
+++ core/xmlConverter.lua
@@ -0,0 +1,37 @@
+local ns = ...
+
+require("luaxml")
+
+local function parse(parent, xmlElement)
+
+	local this = {
+		attributes = {},
+		elements = {},
+		tag = "",
+		parent = nil,
+	}
+
+	this.tag = xmlElement:tag()
+	this.parent = parent
+
+	for key, value in pairs(xmlElement) do
+
+		if type(key) == "number" then
+
+			if tonumber(key) > 0 then
+				table.insert(this.elements, parse(this, value))
+			end
+
+		else
+			this.attributes[key] = value
+		end
+
+	end
+
+	return this
+
+end
+
+ns.xmlConverter = {
+	parse = parse
+}
