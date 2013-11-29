46d0a772916127f9537aaac5d52562228606727d
andy <andy@andydote.co.uk>
2013-11-29 18:14:52 +0000

    api: add table.wipe

---
+++ api/alias.table.lua
@@ -0,0 +1,13 @@
+local ns = ...
+
+--not quite the same as table.wipe, but hey
+local wipe = function(t)
+	
+	for k, v in pairs(t) do
+		t[k] = nil
+	end
+
+end
+
+table.wipe = wipe
+--ns.api.export("table.wipe", wipe)
