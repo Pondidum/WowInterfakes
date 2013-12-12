201af259f7fb6d7561b6ff4c27b3081931d4a123
andy <andy@andydote.co.uk>
2013-12-12 13:09:26 +0000

    makeFrame: rename region to frame due to ocd

--- widgets/makeFrame.lua
+++ widgets/makeFrame.lua
@@ -2,21 +2,21 @@ local ns = ...
 local builder = ns.builder
 local eventRegistry = ns.eventRegistry
 
-builder.applyFrame = function(region) 
+builder.applyFrame = function(frame) 
 
-	region.SetBackdrop = function(self, backdrop)
+	frame.SetBackdrop = function(self, backdrop)
 		self.__storage.backdrop = backdrop
 	end
 
-	region.RegisterEvent = function(self, event)
+	frame.RegisterEvent = function(self, event)
 		eventRegistry.register(self, event)
 	end
 
-	region.CreateTexture = function(self, name, layer, inherits, sublevel)
+	frame.CreateTexture = function(self, name, layer, inherits, sublevel)
 		return builder.createTexture(self, name, layer, inherits, sublevel)
 	end
 
-	region.IgnoreDepth = function(self, enabled)
+	frame.IgnoreDepth = function(self, enabled)
 		self.__storage.ingoreDepth = enabled
 	end
 	
