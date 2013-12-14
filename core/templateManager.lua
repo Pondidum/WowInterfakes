f4c5318116343b61a4ff7f1bdf9b829bd0a867f4
andy <andy@andydote.co.uk>
2013-12-14 17:21:26 +0000

    templateManager: declare table beforehand for recursion later

--- core/templateManager.lua
+++ core/templateManager.lua
@@ -2,45 +2,44 @@ local ns = ...
 local log = ns.log:new("templateManager")
 
 local templates = {}
+local templateManager = {}
 
-local templateManager = {
+templateManager.addTemplate = function(name, handlerChain)
 
-	addTemplate = function(name, handlerChain)
+	log.debug("registering template", name)
+	
+	templates[name] = handlerChain
 
-		log.debug("registering template", name)
-		
-		templates[name] = handlerChain
+end
 
-	end,
+templateManager.apply = function(name, target)
 
-	apply = function(name, target)
+	local handlerChain = templates[name]
 
-		local handlerChain = templates[name]
+	if handlerChain then
 
-		if handlerChain then
+		log.debug("applying", name .. "to" .. target:GetName())
 
-			local first = handlerChain[1]
+		local first = handlerChain[1]
 
-			first.element.attributes.name = target:GetName()
+		first.element.attributes.name = target:GetName()
 
-			for i = 1, #handlerChain do
+		for i = 1, #handlerChain do
 
-				local handler = handlerChain[i]
+			local handler = handlerChain[i]
 
-				local element = handler.element
-				local file = handler.file 
+			local element = handler.element
+			local file = handler.file 
 
-				handler.build(file, element, target)
+			handler.build(file, element, target)
 
-			end
-
-		else
-
-			log.warn("unable to find template", name)
 		end
 
-	end,
+	else
 
-}
+		log.warn("unable to find template", name)
+	end
+
+end
 
 ns.templateManager = templateManager
