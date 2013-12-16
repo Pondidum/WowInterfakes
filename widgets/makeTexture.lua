a3bf696ad336da0aeb52d8c4db3a1b7b89647743
andy <andy@andydote.co.uk>
2013-12-16 12:58:59 +0000

    widgets.texture: implement SetTexture

--- widgets/makeTexture.lua
+++ widgets/makeTexture.lua
@@ -1,6 +1,16 @@
 local ns = ...
 local builder = ns.builder
 
-builder.applyTexture = function(region) 
+builder.applyTexture = function(texture) 
+	
+	texture.SetTexture = function(self, textureOrRed, green, blue, alpha)
+
+		if type(textureOrRed) == "string" then
+			self.__storage.texture = texture
+		else
+			self.__storage.textureColor = { red = textureOrRed, green = green, blue = blue, alpha = alpha}
+		end
+
+	end
 
 end
