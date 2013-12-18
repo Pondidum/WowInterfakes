c75fe7d2ac3fd48879ec9c852e3bfddec8907010
andy <andy@andydote.co.uk>
2013-12-18 23:18:06 +0000

    stack: add simple stack object

---
+++ core/stack.lua
@@ -0,0 +1,29 @@
+local ns = ...
+
+local stack = {
+
+	new = function()
+
+		local storage = {}
+
+		local this = {}
+
+		this.push = function(item)
+			table.insert(storage, item)
+		end
+
+		this.pop = function()
+			return table.remove(#storage)
+		end
+
+		this.tip = function()
+			return storage[#storage]
+		end
+
+		return this 
+
+	end,
+
+}
+
+ns.stack = stack
