62e9a29958d86504021d93bec316062d0cd907bb
andy <andy@andydote.co.uk>
2013-12-19 22:47:48 +0000

    wowinterfakes: run normal load again

--- wowinterfakes.lua
+++ wowinterfakes.lua
@@ -28,23 +28,23 @@ project:new({
 
 		ns.builder.init()
 				
-		-- local parser = ns.parsers.toc
-		-- parser.parse("wow-ui-source\\framexml\\framexml.toc")
+		local parser = ns.parsers.toc
+		parser.parse("wow-ui-source\\framexml\\framexml.toc")
 
-		-- setprinthandler(print)
+		setprinthandler(print)
 		
-		-- ns.eventRegistry.raise("ADDON_LOADED")
-		-- ns.eventRegistry.raise("SPELLS_CHANGED")
-		-- ns.eventRegistry.raise("PLAYER_LOGIN")
-		-- ns.eventRegistry.raise("PLAYER_ENTERING_WORLD")
+		ns.eventRegistry.raise("ADDON_LOADED")
+		ns.eventRegistry.raise("SPELLS_CHANGED")
+		ns.eventRegistry.raise("PLAYER_LOGIN")
+		ns.eventRegistry.raise("PLAYER_ENTERING_WORLD")
 
 		
-		local file = xml.load("test.xml")
-		local content = ns.xmlConverter.parse(file)
+		-- local file = xml.load("test.xml")
+		-- local content = ns.xmlConverter.parse(file)
 
-		local parser = ns.parsers.xml
+		-- local parser = ns.parsers.xml
 
-		parser.parse("", content)
+		-- parser.parse("", content)
 		
 	end,
 
