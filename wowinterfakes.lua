83b352ca12f5c3426faa6a91634744e893b914f1
andy <andy@andydote.co.uk>
2013-12-25 16:26:18 +0000

    wowinterfakes: remove whitespace

--- wowinterfakes.lua
+++ wowinterfakes.lua
@@ -1,6 +1,6 @@
 local project = require("project")
 
-local print = print 
+local print = print
 
 local wowinterfakes = project:new({
 
@@ -20,7 +20,7 @@ local wowinterfakes = project:new({
 	end),
 
 	run = function(ns)
-		
+
 		ns.log.config.enabled = true
 		ns.log.config.level = "debug"
 		-- ns.log.config:addFilters(
@@ -29,25 +29,25 @@ local wowinterfakes = project:new({
 		-- )
 
 		ns.builder.init()
-				
+
 		local parser = ns.parsers.toc
 		parser.parse("wow-ui-source\\framexml\\framexml.toc")
 
 		setprinthandler(print)
-		
+
 		ns.eventRegistry.raise("ADDON_LOADED")
 		ns.eventRegistry.raise("SPELLS_CHANGED")
 		ns.eventRegistry.raise("PLAYER_LOGIN")
 		ns.eventRegistry.raise("PLAYER_ENTERING_WORLD")
 
-		
+
 		-- local file = xml.load("test.xml")
 		-- local content = ns.xmlConverter.parse(file)
 
 		-- local parser = ns.parsers.xml
 
 		-- parser.parse("", content)
-		
+
 	end,
 
 })
