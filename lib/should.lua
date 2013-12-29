fbe6a4870846e179992217cd3fa5faed7c3c9137
andy <andy@andydote.co.uk>
2013-12-29 12:56:46 +0000

    libs: update should to v1.0.0

--- lib/should.lua
+++ lib/should.lua
@@ -1,4 +1,4 @@
-local VERSION = "0.0.1"
+local VERSION = "1.0.0"
 
 local should = {
 
@@ -10,31 +10,39 @@ local should = {
 			actual = actual + 1
 		end
 
-		assert(actual == expected, string.format(message or "Expected %d elements, but there were %d.", expected, actual))
+		assert(actual == expected, string.format(message or "Expected %s elements, but there were %s.",
+												 tostring(expected),
+												 tostring(actual)))
 
 	end,
 
 	equal = function(expected, actual, message)
 
-		assert(actual == expected, string.format(message or "Expected %s and %s to be the same, but were different.", expected, actual))
+		assert(actual == expected, string.format(message or "Expected '%s' and '%s' to be the same, but were different.",
+												 tostring(expected),
+												 tostring(actual)))
 
 	end,
 
 	notEqual = function(expected, actual, message)
 
-		assert(actual ~= expected, string.format(message or "Expected %s and %s to be different, but were the same.", expected, actual))
+		assert(actual ~= expected, string.format(message or "Expected '%s' and '%s' to be different, but were the same.",
+												 tostring(expected),
+												 tostring(actual)))
 
 	end,
 
 	haveKey = function(item, collection, message)
 
-		assert(collection[item], string.format(message or "Expected the key %s to exist, but didn't.", item))
+		assert(collection[item], string.format(message or "Expected the key '%s' to exist, but didn't.",
+											   tostring(item)))
 
 	end,
 
 	notHaveKey = function(item, collection, message)
 
-		assert(collection[item] == nil, string.format(message or "Expected the key %s to exist, but didn't.", item))
+		assert(collection[item] == nil, string.format(message or "Expected the key '%s' to not exist, but didn't.",
+													  tostring(item)))
 
 	end,
 
@@ -48,7 +56,8 @@ local should = {
 			end
 		end
 
-		assert(inCollection, string.format(message or "Expected %s to be in the collection, but it wasn't.", item))
+		assert(inCollection, string.format(message or "Expected '%s' to be in the collection, but it wasn't.",
+										   tostring(item)))
 
 	end,
 
@@ -62,7 +71,37 @@ local should = {
 			end
 		end
 
-		assert(inCollection == false, string.format(message or "Expected %s to not be in the collection, but it was.", item))
+		assert(inCollection == false, string.format(message or "Expected '%s' to not be in the collection, but it was.",
+													tostring(item)))
+
+	end,
+
+	beTrue = function(item, message)
+
+		assert(item == true, string.format(message or "Expected '%s' to be True, but it wasn't.",
+										   tostring(item)))
+
+	end,
+
+	beFalse = function(item, message)
+
+		assert(item == false, string.format(message or "Expected '%s' to be True, but it wasn't.",
+											tostring(item)))
+
+	end,
+
+	beNil = function(item, message)
+
+		assert(item == nil, string.format(message or "Expected '%s' to be nil, but it was a %s.",
+										  tostring(item),
+										  type(item)))
+
+	end,
+
+	notBeNil = function(item, message)
+
+		assert(item, string.format(message or "Expected item to not be nil, but it was.",
+								   tostring(item)))
 
 	end,
 
