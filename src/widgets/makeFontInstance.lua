a4e17e11c04205751fdd5bc667602486c059ab23
andy <andy@andydote.co.uk>
2013-12-30 18:05:55 +0000

    widgets.fontInstance: implement setSpacing

--- src/widgets/makeFontInstance.lua
+++ src/widgets/makeFontInstance.lua
@@ -1,12 +1,12 @@
 local ns = ...
 local builder = ns.builder
 
-builder.applyFontInstance = function(font) 
-	
+builder.applyFontInstance = function(font)
+
 	font.GetFont = function(self)
 
 		local data = self.__storage.font
-		
+
 		if not data then
 			return nil
 		end
@@ -14,5 +14,9 @@ builder.applyFontInstance = function(font)
 		return data.name, data.height, data.flags
 
 	end
-	
+
+	font.SetSpacing = function(self, spacing)
+		self.__storage.spacing = spacing
+	end
+
 end
