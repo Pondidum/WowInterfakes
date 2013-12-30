d54612fe512f973202ff4bec2f2fac56834f2431
andy <andy@andydote.co.uk>
2013-12-30 17:52:56 +0000

    tags.anchor: update default args to mirror ui better

--- src/parsers/xml/anchor.lua
+++ src/parsers/xml/anchor.lua
@@ -2,7 +2,7 @@ local ns = ...
 local log = ns.log:new("anchors")
 
 local fixValue = function(value)
-	
+
 	if value == nil then
 		return nil
 	end
@@ -17,17 +17,18 @@ end
 local tag = ns.parsers.xmlTag:new({
 
 	processChildren = false,
-	
+
 	build = function(self, file, element, target)
 
 		local point = element.attributes.point or ""
 
-		log.debug(string.format("applying anchor %s to %s", point, target:GetName()))
+		local relativeFrame = element.attributes.relativeTo or target:GetParent()
+		local relativePoint = element.attributes.relativePoint or point
 
-		local relativeFrame = element.attributes.relativeTo or ""
-		local relativePoint = element.attributes.relativePoint or  ""
 		local x = fixValue(element.attributes.x) or 0
 		local y = fixValue(element.attributes.y) or 0
+
+		log.debug(string.format("applying anchor %s.%s to %s.%s", target:GetName(), point, tostring(relativeFrame), relativePoint))
 
 		target:SetPoint(point, relativeFrame, relativePoint, x, y)
 
