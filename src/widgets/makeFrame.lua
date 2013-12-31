099742ce4c946fcd258ebb035e7329de696a0bc7
andy <andy@andydote.co.uk>
2013-12-31 15:07:34 +0000

    apply.frame: update to new builder

--- src/widgets/makeFrame.lua
+++ src/widgets/makeFrame.lua
@@ -2,42 +2,46 @@ local ns = ...
 local builder = ns.builder
 local eventRegistry = ns.eventRegistry
 
-builder.applyFrame = function(frame) 
+ns.widgetMetaBuilder.addType({
+	name = "Frame",
+	extends = { "ScriptObject", "VisibleRegion" },
+	build = function(target)
 
-	frame.SetBackdrop = function(self, backdrop)
-		self.__storage.backdrop = backdrop
-	end
+		target.SetBackdrop = function(self, backdrop)
+			self.__storage.backdrop = backdrop
+		end
 
-	frame.SetBackdropBorderColor = function(self, red, green, blue, alpha)
-		self.__storage.backdropBorderColor = { red = red, green = green, blue = blue, alpha = alpha }
-	end
+		target.SetBackdropBorderColor = function(self, red, green, blue, alpha)
+			self.__storage.backdropBorderColor = { red = red, green = green, blue = blue, alpha = alpha }
+		end
 
-	frame.SetBackdropColor = function(self, red, green, blue, alpha)
-		self.__storage.backdropColor = { red = red, green = green, blue = blue, alpha = alpha }
-	end
+		target.SetBackdropColor = function(self, red, green, blue, alpha)
+			self.__storage.backdropColor = { red = red, green = green, blue = blue, alpha = alpha }
+		end
 
-	frame.RegisterEvent = function(self, event)
-		eventRegistry.register(self, event)
-	end
+		target.RegisterEvent = function(self, event)
+			eventRegistry.register(self, event)
+		end
 
-	frame.CreateTexture = function(self, name, layer, inherits, sublevel)
-		return builder.createTexture(self, name, layer, inherits, sublevel)
-	end
+		target.CreateTexture = function(self, name, layer, inherits, sublevel)
+			return builder.createTexture(self, name, layer, inherits, sublevel)
+		end
 
-	frame.CreateFontString = function(self, name, layer, inherits)
-		return builder.createFontString(self, name, layer, inherits)
-	end
+		target.CreateFontString = function(self, name, layer, inherits)
+			return builder.createFontString(self, name, layer, inherits)
+		end
 
-	frame.IgnoreDepth = function(self, enabled)
-		self.__storage.ingoreDepth = enabled
-	end
-	
-	frame.GetFrameLevel = function(self)
-		return self.__storage.frameLevel or 1
-	end
+		target.IgnoreDepth = function(self, enabled)
+			self.__storage.ingoreDepth = enabled
+		end
 
-	frame.SetFrameLevel = function(self, value)
-		self.__storage.frameLevel = value
-	end
+		target.GetFrameLevel = function(self)
+			return self.__storage.frameLevel or 1
+		end
 
-end
+		target.SetFrameLevel = function(self, value)
+			self.__storage.frameLevel = value
+		end
+
+	end,
+})
