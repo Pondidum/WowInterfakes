31f13b9917eed4484cf71b0554f687bebd6ae438
andy <andy@andydote.co.uk>
2013-12-31 15:12:20 +0000

    apply.texture: update to new builder

--- src/widgets/makeTexture.lua
+++ src/widgets/makeTexture.lua
@@ -1,67 +1,71 @@
 local ns = ...
 local builder = ns.builder
 
-builder.applyTexture = function(texture)
+ns.widgetMetaBuilder.addType({
+	name = "Texture",
+	extends = { "LayeredRegion" },
+	build = function(target)
 
-	texture.SetTexture = function(self, textureOrRed, green, blue, alpha)
+		target.SetTexture = function(self, textureOrRed, green, blue, alpha)
 
-		if type(textureOrRed) == "string" then
-			self.__storage.texture = texture
-		else
-			self.__storage.textureColor = { red = textureOrRed, green = green, blue = blue, alpha = alpha}
-		end
-
-	end
-
-	texture.SetTexCoord = function(self, leftOrULx, rightOrULy, topOrLLx, bottomOrLLy, URx, URy, LRx, LRy)
-
-		if URx == nil or URy == nil or LRx == nil or LRy then
-
-			local l = leftOrULx
-			local r = rightOrULy
-			local t = topOrLLx
-			local b = bottomOrLLy
-
-			self.__storage.texCoord = {
-				ULx = l,
-				ULy = t,
-				LLx = l,
-				LLy = b,
-				URx = r,
-				URy = t,
-				LRx = r,
-				LRy = b,
-			}
-
-		else
-
-			self.__storage.texCoord = {
-				ULx = ULx,
-				ULy = ULy,
-				LLx = LLx,
-				LLy = LLy,
-				URx = URx,
-				URy = URy,
-				LRx = LRx,
-				LRy = LRy,
-			}
+			if type(textureOrRed) == "string" then
+				self.__storage.texture = texture
+			else
+				self.__storage.textureColor = { red = textureOrRed, green = green, blue = blue, alpha = alpha}
+			end
 
 		end
 
-	end
+		target.SetTexCoord = function(self, leftOrULx, rightOrULy, topOrLLx, bottomOrLLy, URx, URy, LRx, LRy)
 
-	texture.GetTexCoord = function(self)
+			if URx == nil or URy == nil or LRx == nil or LRy then
 
-		local c = self.__storage.texCoord
+				local l = leftOrULx
+				local r = rightOrULy
+				local t = topOrLLx
+				local b = bottomOrLLy
 
-		if c then
-			return c.ULx, c.ULy, c.LLx, c.LLy, c.URx, c.URy, c.LRx, c.LRy
+				self.__storage.texCoord = {
+					ULx = l,
+					ULy = t,
+					LLx = l,
+					LLy = b,
+					URx = r,
+					URy = t,
+					LRx = r,
+					LRy = b,
+				}
+
+			else
+
+				self.__storage.texCoord = {
+					ULx = ULx,
+					ULy = ULy,
+					LLx = LLx,
+					LLy = LLy,
+					URx = URx,
+					URy = URy,
+					LRx = LRx,
+					LRy = LRy,
+				}
+
+			end
+
 		end
 
-	end
+		target.GetTexCoord = function(self)
 
-	texture.SetRotation = function(self, radians)
-		self.__storage.rotation = radians
-	end
+			local c = self.__storage.texCoord
 
-end
+			if c then
+				return c.ULx, c.ULy, c.LLx, c.LLy, c.URx, c.URy, c.LRx, c.LRy
+			end
+
+		end
+
+		target.SetRotation = function(self, radians)
+			self.__storage.rotation = radians
+		end
+
+	end,
+})
