dde26d9fb086647fffd57cdcdbaf5c0b48b35b62
andy <andy@andydote.co.uk>
2013-12-31 15:05:05 +0000

    apply.visibleRegion: update to new builder

--- src/widgets/makeVisibleRegion.lua
+++ src/widgets/makeVisibleRegion.lua
@@ -1,30 +1,34 @@
 local ns = ...
 local builder = ns.builder
 
-builder.applyVisibleRegion = function(region)
- 
-	region.GetAlpha = function(self)
-		return self.__storage.alpha
-	end
-	
-	region.SetAlpha = function(self, value)
-		self.__storage.alpha = value
-	end	
- 
-	region.Hide = function(self)
-		self.__storage.visible = false
-	end
-	
-	region.Show = function(self)
-		self.__storage.visible = true
-	end
-	
-	region.IsShown = function(self)
-		return self.__storage.visible
-	end
-	
-	region.IsVisible = function(self)
-		return self.__storage.visible
-	end
-	
-end
+ns.widgetMetaBuilder.addType({
+	name = "VisibleRegion",
+	extends = { "Region" },
+	build = function(target)
+
+		target.GetAlpha = function(self)
+			return self.__storage.alpha
+		end
+
+		target.SetAlpha = function(self, value)
+			self.__storage.alpha = value
+		end
+
+		target.Hide = function(self)
+			self.__storage.visible = false
+		end
+
+		target.Show = function(self)
+			self.__storage.visible = true
+		end
+
+		target.IsShown = function(self)
+			return self.__storage.visible
+		end
+
+		target.IsVisible = function(self)
+			return self.__storage.visible
+		end
+
+	end,
+})
