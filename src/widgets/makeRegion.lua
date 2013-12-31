f8b83a974353bdad6cf90a3c69a2e8f0f443526d
andy <andy@andydote.co.uk>
2013-12-31 14:56:47 +0000

    apply.region: update to new builder

--- src/widgets/makeRegion.lua
+++ src/widgets/makeRegion.lua
@@ -1,107 +1,111 @@
 local ns = ...
 local builder = ns.builder
 
-builder.applyRegion = function(region)
+ns.widgetMetaBuilder.addType({
+	name = "Region",
+	extends = { "ParentedObject" },
+	build = function(target)
 
-	region.GetWidth = function(self)
-		return self.__storage.width
-	end
+		target.GetWidth = function(self)
+			return self.__storage.width
+		end
 
-	region.GetHeight = function(self)
-		return self.__storage.height
-	end
+		target.GetHeight = function(self)
+			return self.__storage.height
+		end
 
-	region.GetSize = function(self)
-		return self.__storage.width, self.__storage.height
-	end
+		target.GetSize = function(self)
+			return self.__storage.width, self.__storage.height
+		end
 
-	region.SetWidth = function(self, value)
-		self.__storage.width = value
-	end
+		target.SetWidth = function(self, value)
+			self.__storage.width = value
+		end
 
-	region.SetHeight = function(self, value)
-		self.__storage.height = value
-	end
+		target.SetHeight = function(self, value)
+			self.__storage.height = value
+		end
 
-	region.SetSize = function(self, w, h)
-		self.__storage.width = w
-		self.__storage.height = h
-	end
+		target.SetSize = function(self, w, h)
+			self.__storage.width = w
+			self.__storage.height = h
+		end
 
-	region.ClearAllPoints = function(self, value)
-		self.__storage.points = {}
-	end
+		target.ClearAllPoints = function(self, value)
+			self.__storage.points = {}
+		end
 
-	region.SetPoint = function(self, anchor, other, otherAnchor, x, y)
-		self.__storage.points = self.__storage.points or {}
+		target.SetPoint = function(self, anchor, other, otherAnchor, x, y)
+			self.__storage.points = self.__storage.points or {}
 
-		local points = self.__storage.points
+			local points = self.__storage.points
 
-		local index = nil
+			local index = nil
 
-		for i, point in ipairs(points) do
-			if point.anchor == anchor then
-				index = i
+			for i, point in ipairs(points) do
+				if point.anchor == anchor then
+					index = i
+				end
 			end
+
+			local data = {
+				anchor = anchor,
+				other = other,
+				otherAnchor = otherAnchor,
+				x = x,
+				y = y
+			}
+
+			if index == nil then
+				table.insert(points, data)
+			else
+				points[index] = data
+			end
+
 		end
 
-		local data = {
-			anchor = anchor,
-			other = other,
-			otherAnchor = otherAnchor,
-			x = x,
-			y = y
-		}
-
-		if index == nil then
-			table.insert(points, data)
-		else
-			points[index] = data
+		target.SetAllPoints = function(self, otherRegion)
+			self:SetPoint("TOPLEFT", otherRegion, "TOPLEFT", 0, 0)
+			self:SetPoint("BOTTOMRIGHT", otherRegion, "BOTTOMRIGHT", 0, 0)
 		end
 
-	end
+		target.GetNumPoints = function(self)
 
-	region.SetAllPoints = function(self, otherRegion)
-		self:SetPoint("TOPLEFT", otherRegion, "TOPLEFT", 0, 0)
-		self:SetPoint("BOTTOMRIGHT", otherRegion, "BOTTOMRIGHT", 0, 0)
-	end
+			if self.__storage.points == nil then
+				return 0
+			end
 
-	region.GetNumPoints = function(self)
+			local count = 0
 
-		if self.__storage.points == nil then
-			return 0
+			-- it is indexed, but use pairs() so we dont miss keys
+			for k,v in pairs(self.__storage.points) do
+				count = count + 1
+			end
+
+			return count
+
 		end
 
-		local count = 0
+		target.GetPoint = function(self, index)
+			self.__storage.points = self.__storage.points or {}
 
-		-- it is indexed, but use pairs() so we dont miss keys
-		for k,v in pairs(self.__storage.points) do
-			count = count + 1
+			local data = self.__storage.points[index]
+
+			if data == nil then
+				return nil
+			else
+				return data.anchor, data.other, data.otherAnchor, data.x, data.y
+			end
+
 		end
 
-		return count
-
-	end
-
-	region.GetPoint = function(self, index)
-		self.__storage.points = self.__storage.points or {}
-
-		local data = self.__storage.points[index]
-
-		if data == nil then
-			return nil
-		else
-			return data.anchor, data.other, data.otherAnchor, data.x, data.y
+		target.SetParent = function(self, target)
+			self.__storage.parent = target
 		end
 
-	end
+		target.CreateAnimationGroup = function(self, name, inherits)
+			return builder.createAnimationGroup(self, name, inherits)
+		end
 
-	region.SetParent = function(self, target)
-		self.__storage.parent = target
-	end
-
-	region.CreateAnimationGroup = function(self, name, inherits)
-		return builder.createAnimationGroup(self, name, inherits)
-	end
-
-end
+	end,
+})
