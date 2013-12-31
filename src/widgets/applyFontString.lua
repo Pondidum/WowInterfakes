eca14848e7871d7ffa2dee8ab176dec187c1d37c
andy <andy@andydote.co.uk>
2013-12-31 11:50:26 +0000

    apply.fontString: update to new builder

---
+++ src/widgets/applyFontString.lua
@@ -0,0 +1,14 @@
+local ns = ...
+local builder = ns.builder
+
+ns.widgetMetaBuilder.addType({
+	name = "FontString",
+	extends = { "FontInstance" },
+	build = function(target)
+
+		target.SetText = function(self, text)
+			self.__storage.text = text
+		end
+
+	end,
+})
