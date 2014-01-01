f0125323a00758412496c33daec0dad52f79e11d
andy <andy@andydote.co.uk>
2014-01-01 13:02:57 +0000

    apply.dressUpModel: implement subtype

---
+++ src/widgets/applyDressUpModel.lua
@@ -0,0 +1,11 @@
+local ns = ...
+local builder = ns.builder
+local eventRegistry = ns.eventRegistry
+
+ns.widgetMetaBuilder.addType({
+	name = "DressUpModel",
+	extends = { "PlayerModel" },
+	build = function(target)
+
+	end,
+})
