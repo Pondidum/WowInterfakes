b9f63051feb28136f3cd134b779ccd2f7adf9f67
andy <andy@andydote.co.uk>
2014-01-04 18:21:03 +0000

    api: export setSelectedScreenResolutionIndex

---
+++ src/api/setSelectedScreenResolutionIndex.lua
@@ -0,0 +1,7 @@
+local ns = ...
+
+local setSelectedScreenResolutionIndex = function(index)
+	-- do some c++ magic apparently.
+end
+
+ns.api.export("SetSelectedScreenResolutionIndex", setSelectedScreenResolutionIndex)
