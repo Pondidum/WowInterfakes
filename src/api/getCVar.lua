f3a366a7dd49997f91578a17fd48325f28c8961f
andy <andy@andydote.co.uk>
2014-01-05 15:16:26 +0000

    api.getCvar: log what cvars are queryed

--- src/api/getCVar.lua
+++ src/api/getCVar.lua
@@ -1,10 +1,17 @@
 local ns = ...
+local log = ns.log:new("api.getCVar")
 
 local cvars = {
-	["gxMonitor"] = 0,
+	["gxMonitor"] = 1,
+	["waterDetail"] = 1,
+	["rippleDetail"] = 1,
+	["reflectionMode"] = 3,
 }
 
 local getCVar = function(name)
+
+	log.debug(name, cvars[name])
+
 	return tostring(cvars[name] or "")
 end
 
