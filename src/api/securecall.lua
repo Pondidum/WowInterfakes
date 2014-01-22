55743854ff77da6f43764a3404bb65214ac76875
andy <andy@andydote.co.uk>
2014-01-22 22:39:48 +0000

    api.securecall: if passed a string, lookup in _G for the method

--- src/api/securecall.lua
+++ src/api/securecall.lua
@@ -1,7 +1,13 @@
 local ns = ...
 
 local securecall = function(target, ...)
+
+	if type(target) == "string" then
+		target = _G[target]
+	end
+
 	return target(...)
+
 end
 
 ns.api.export("securecall", securecall)
