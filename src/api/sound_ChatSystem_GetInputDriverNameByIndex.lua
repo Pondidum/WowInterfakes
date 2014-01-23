25e7a81f947abac5b28140f701ba048ed6da6637
andy <andy@andydote.co.uk>
2014-01-23 21:30:54 +0000

    api: implement Sound_ChatSystem_GetInputDriverNameByIndex

---
+++ src/api/sound_ChatSystem_GetInputDriverNameByIndex.lua
@@ -0,0 +1,7 @@
+local ns = ...
+
+local sound_ChatSystem_GetInputDriverNameByIndex = function()
+	return "Default"
+end
+
+ns.api.export("Sound_ChatSystem_GetInputDriverNameByIndex", sound_ChatSystem_GetInputDriverNameByIndex)
