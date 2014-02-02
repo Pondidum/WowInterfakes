3a8f69debb5229eb42d8f2c5652f9ee6ef881cb0
andy <andy@andydote.co.uk>
2014-02-02 11:48:40 +0000

    widgets.cooldown: implement SetCooldown

--- src/widgets/applyCooldown.lua
+++ src/widgets/applyCooldown.lua
@@ -5,5 +5,10 @@ ns.builder.addType({
 	extends = { "Frame" },
 	build = function(target)
 
+		target.SetCooldown = function(self, start, duration)
+			self.__storage.start = start
+			self.__storage.duration = duration
+		end
+
 	end,
 })
