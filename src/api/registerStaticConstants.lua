10a1fc6b0296ff087213b79bc71f563efb21462f
andy <andy@andydote.co.uk>
2014-02-10 23:20:42 +0000

    api: register NUM_LE_RAID_BUFF_TYPES static constant

--- src/api/registerStaticConstants.lua
+++ src/api/registerStaticConstants.lua
@@ -7,6 +7,7 @@ local registerStaticConstants = function()
 	LE_LFG_CATEGORY_SCENARIO = 4
 
 	NUM_LE_LFG_CATEGORYS = 4		--? needs to be checked in wow
+	NUM_LE_RAID_BUFF_TYPES = 8
 end
 
 ns.api.export("RegisterStaticConstants", registerStaticConstants)
