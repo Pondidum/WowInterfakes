bd354f68aea5a309921b91cf6159bfbc8fe1d985
andy <andy@andydote.co.uk>
2014-03-05 22:46:53 +0000

    widgets.scrollFrame: implement getScrollChild

--- src/widgets/applyScrollFrame.lua
+++ src/widgets/applyScrollFrame.lua
@@ -7,11 +7,16 @@ ns.builder.addType({
 	extends = { "Frame" },
 	build = function(target)
 
+		target.GetScrollChild = function(self)
+			return self.__storage.scrollChild
+		end
+
 	end,
 	initInstance = function(target)
 
 		local bar = builder.createFrame("Slider", target:GetName() .. "ScrollBar", target)
 		local down = builder.createFrame("Button", target:GetName() .. "ScrollBarScrollDownButton", target)
 		local up = builder.createFrame("Button", target:GetName() .. "ScrollBarScrollUpButton", target)
+
 	end,
 })
