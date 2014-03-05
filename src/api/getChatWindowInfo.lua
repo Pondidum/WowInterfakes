43d90dcd305bf4f9c5a5c52983790e9965205288
andy <andy@andydote.co.uk>
2014-03-05 12:51:15 +0000

    api.getChatWindow: return full frame name

--- src/api/getChatWindowInfo.lua
+++ src/api/getChatWindowInfo.lua
@@ -1,7 +1,7 @@
 local ns = ...
 
 local getChatWindowInfo = function(index)
-	return "ChatFrame1", 12, 1, 1, 1, 0, true, true, true, nil
+	return "ChatFrame" ..index, 12, 1, 1, 1, 0, true, true, true, nil
 end
 
 ns.api.export("GetChatWindowInfo", getChatWindowInfo)
