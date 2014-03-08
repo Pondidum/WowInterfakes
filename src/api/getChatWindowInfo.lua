8180eecfb920c3c204bb48a183312c181831707b
andy <andy@andydote.co.uk>
2014-03-08 14:44:12 +0000

    api.getChatWindowInfo: use 1+nil for true+false values

--- src/api/getChatWindowInfo.lua
+++ src/api/getChatWindowInfo.lua
@@ -1,7 +1,8 @@
 local ns = ...
 
 local getChatWindowInfo = function(index)
-	return "ChatFrame" ..index, 12, 1, 1, 1, 0, true, true, true, nil
+		--name, fontSize, r, g, b, a, shown, locked, docked, uninteractable
+	return "ChatFrame" ..index, 12, 1, 1, 1, 0, true, true, nil, nil
 end
 
 ns.api.export("GetChatWindowInfo", getChatWindowInfo)
